3b6302649d6cf532447449fe2ac7a29e25a57a4b
Sreenivassreeee <ksreenivascse@gmail.com>
2023-07-13 02:04:16 +0530

    CORS added for frontend temporary purpose that is http://localhost:3000

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 import { GlobalService } from '@libs/libs';
+import * as cors from 'cors';
 
 async function bootstrap() {
   GlobalService.appName = 'near-me';
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+  });
 
   await app.listen(process.env.PORT);
   new Logger('Bootstrap').verbose('Running At ' + process.env.PORT);
