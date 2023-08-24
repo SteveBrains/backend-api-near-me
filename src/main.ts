2124224b34d9022db0e0116ff66a23b0925570f2
Sreenivassreeee <ksreenivascse@gmail.com>
2023-08-24 15:56:25 +0530

    deplyment fix

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,10 @@ async function bootstrap() {
   GlobalService.appName = 'near-me';
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000'],
+    methods: ['GET', 'POST', 'OPTIONS'],
+    credentials: true,
+    maxAge: 600,
+    origin: ['*'],
   });
 
   await app.listen(process.env.PORT);
