075afc173215aa24a52ad8e5b8fc122c9831531f
Sreenivassreeee <ksreenivascse@gmail.com>
2023-08-24 17:13:58 +0530

    CROS updated

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,13 @@ import * as cors from 'cors';
 async function bootstrap() {
   GlobalService.appName = 'near-me';
   const app = await NestFactory.create(AppModule);
+  const origins = [...process.env.PLAYGROUND_URL.split('|')].map((x) =>
+    x.trim(),
+  );
   app.enableCors({
     methods: ['GET', 'POST'],
-    origin: [process.env.PLAYGROUND_URL],
+    origin: [...origins],
   });
-
   await app.listen(process.env.PORT);
   new Logger('Bootstrap').verbose('Running At ' + process.env.PORT);
 }
